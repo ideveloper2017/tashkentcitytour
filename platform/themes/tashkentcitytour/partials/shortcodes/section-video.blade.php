db7dc64e4fea4486107af0ead6fdf1e7d9442818
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-08-25 08:28:26 +0500

    commmit

--- platform/themes/tashkentcitytour/partials/shortcodes/section-video.blade.php
+++ platform/themes/tashkentcitytour/partials/shortcodes/section-video.blade.php
@@ -1,8 +1,8 @@
 <div class="video-section">
-    <div class="video-bg set-bg" data-setbg="{!! Theme::asset()->url('img/video-bg.jpg') !!}"></div>
+    <div class="video-bg set-bg" data-setbg="{!! Theme::asset()->url('img/banner01-eb1d759278.jpg') !!}"></div>
     <div class="container">
         <div class="video-text set-bg" data-setbg="{!! Theme::asset()->url('img/video-inside-bg.jpg') !!}">
-            <a href="https://www.youtube.com/watch?v=j56YlCXuPFU" class="pop-up"><i class="fa fa-play"></i></a>
+            <a href="https://www.youtube.com/watch?v=AS_lYmmWon0" class="pop-up"><i class="fa fa-play"></i></a>
         </div>
     </div>
 </div>
