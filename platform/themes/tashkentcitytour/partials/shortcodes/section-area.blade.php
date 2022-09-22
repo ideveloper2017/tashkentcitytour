0914d0e64927e478c3ff29d184db696bf3115680
Bahrom Parpiyev <bahrom1982@gmail.com>
2022-09-22 10:54:42 +0500

    commmit

--- platform/themes/tashkentcitytour/partials/shortcodes/section-area.blade.php
+++ platform/themes/tashkentcitytour/partials/shortcodes/section-area.blade.php
@@ -1,11 +1,25 @@
-<section class="hero-area set-bg {!! $style !!}" data-setbg="{!! Theme::asset()->url('img/banner01-eb1d759278.png')!!}">
+<section class="hero-area set-bg">
     <div class="container">
         <div class="row">
-            <div class="col-lg-12 text-center">
-                <div class="hero-text">
-                    <h1>{!! clean($title) !!}</h1>
-                </div>
-            </div>
+<div class="ri-slider-item">
+    <div class="ri-sliders owl-carousel">
+        <div class="single-img set-bg" data-setbg="{!! Theme::asset()->url('img/banner01-eb1d759278.png')!!}"></div>
+        <div class="single-img set-bg" data-setbg="{!! Theme::asset()->url('img/banner01-eb1d759278.png')!!}"></div>
+        <div class="single-img set-bg" data-setbg="{!! Theme::asset()->url('img/banner01-eb1d759278.png')!!}"></div>
+    </div>
+</div>
         </div>
     </div>
 </section>
+
+{{--<section class="hero-area set-bg {!! $style !!}" data-setbg="{!! Theme::asset()->url('img/banner01-eb1d759278.png')!!}">--}}
+{{--    <div class="container">--}}
+{{--        <div class="row">--}}
+{{--            <div class="col-lg-12 text-center">--}}
+{{--                <div class="hero-text">--}}
+{{--                    <h1>{!! clean($title) !!}</h1>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--</section>--}}
